b1e708b2021617930942828a96d1a61e08f0a54c
KenBoi26 <kennethphilip23@lpu.in>
2025-01-22 20:05:26 +0530

    add comments to helloWorld.java for clarity on compilation and class structure

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,16 @@
+// to compile the Java code type javac <file name>.java in the terminal
+// and makes a .class file, this contains the byte code
+// java .\<file name>.java or java <class name> to run the code
+
+
+
+import java.util.*;
 public class helloWorld {
+    // public ensure that the class can be accessed outisde the package
+    // static means that the method belongs to the class and not any object of the class
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
     
 }
+

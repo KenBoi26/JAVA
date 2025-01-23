f8517713739d81b8a0910ab933178ffabbff1b37
KenBoi26 <kennethphilip23@lpu.in>
2025-01-23 18:43:26 +0530

    add ternary_operators.java to demonstrate conditional logic with user input

---
+++ ternary_operators.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ternary_operators {
+    public static void main(String[] args) {
+        //Type your code here
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        int pos = (num1 > 0 && num2 > 0) ? 1 : 0;
+        int div = (num1%2 == 0 || num2%2 == 0) ? 0 : 1;
+        
+        String result = (pos == 1 && div == 1) ? "Both integers are positive and not multiples of 2" : "The condition is not met for both integers";
+        
+        System.out.println(result);
+        
+    }
+}
+

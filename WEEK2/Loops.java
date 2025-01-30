05e41b10aa138b9c2b3ecc4457f4d6c1e62f59d9
KenBoi26 <kennethphilip23@lpu.in>
2025-01-30 19:31:37 +0530

    add additional main method for calculating the sum of squares of odd digits

--- WEEK2/Loops.java
+++ WEEK2/Loops.java
@@ -5,7 +5,7 @@ public class Loops {
 
 
 
-    public static void main(String[] args){
+    public static void main1(String[] args){
         Scanner sc = new Scanner(System.in);
         
         int num = sc.nextInt();
@@ -25,6 +25,23 @@ public class Loops {
         }else{
             System.out.printf("The number of digits in %d does not match the sum of its digits.", og);
         }
+    }
+
+    public static void main2(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        
+        
+        do{
+            if(num%2 != 0){
+                sum += (num%10)*(num%10);
+            }
+            
+            num/=10;
+        }while(num > 0);
+        
+        System.out.printf("%d", sum);
     }
 
 }
